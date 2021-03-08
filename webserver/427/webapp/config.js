cd90e6f019ae1ed46646963b0eb4edaf5533f5f4
mtinti <michele.tinti@gmail.com>
2021-03-08 16:41:28 +0000

    add webserver

---
+++ webserver/427/webapp/config.js
@@ -0,0 +1,25 @@
+let X1='logFC';
+let Y1='-log10FDR';
+let X2='logCPM';
+let Y2='logFC';
+
+let FLIP_Y1=false;
+let FLIP_Y2=false;
+
+let PLOT_WIDTH=550;
+let PLOT_HEIGHT=400;
+
+let PROJECT_TITLE='RBP5 OE';
+
+let margin = 0.02; //used in add_margin.js
+
+let BOX_PLOT_COLS_1 = ['C1','C2','C3'];
+let BOX_PLOT_COLS_2 = ['T1','T2','T3'];
+
+let CONDITION_1 = 'Control';
+let CONDITION_2 = 'OE';
+
+let COLOR_SELECTION_TABLE = 'orange';
+let COLOR_SELECTION_CIRCLE = 'orange';
+
+let Y_LABEL_BARPLOT = 'logCPM';
